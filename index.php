113c2f0121741fc909ef2f049893f4e54b2e81a1
luisabauer <luisbauer@hotmail.com>
2022-08-09 22:46:34 -0400

    segunda

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "localhost";
 $username = "root";
-$password = "Senha123";
+$password = "teste";
 $database = "meubanco";
 
 // Criar conexão
